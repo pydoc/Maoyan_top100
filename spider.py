23389dbe96e3b79ac8fe94c6b56bee0ee626382f
pydoc <946517875@qq.com>
2018-08-06 22:14:48 +0800

    Initial commit

---
+++ spider.py
@@ -0,0 +1,50 @@
+import requests
+from requests import RequestException
+import re
+import json
+from multiprocessing import Pool
+
+def get_one_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?' +
+                         'name.*?title="(.*?)".*?star">(.*?)</p>.*?releasetime">' +
+                         '(.*?)</p>.*?score.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>',re.S)
+    items = re.findall(pattern,html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score': item[5] + item[6],
+        }
+
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f: #用utf-8编码
+        f.write(json.dumps(content, ensure_ascii=False) + '\n') #不使用ascii码写入json
+        f.close()
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+if __name__ == '__main__':
+    for i in range(10):
+        main(i*10)
+
+#if __name__ == '__main__':
+#    pool = Pool()
+#    pool.map(main, [i*10 for i in range(10)])
+    # 可以提供指定数量的进程池，用户调用，如果有新的请求到进程池且还没满，就会创建一个新的进程执行进程，如果池满了就执行等待
